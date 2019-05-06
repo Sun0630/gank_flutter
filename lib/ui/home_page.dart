b720a8243ea429f352029c8b449d5472c10b388c
sunxin <sunxin0630@126.com>
2019-05-06 21:59:44 +0800

    1. init drawer

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gank_flutter/widget/widget_cion_font.dart';
+import 'package:gank_flutter/widget/widget_gank_drawer.dart';
 
 class HomePage extends StatefulWidget {
   static const String ROUTE_NAME = 'home';
@@ -8,18 +10,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        centerTitle: true,
-        title: Offstage(
-          offstage: false,
-          child: Text('今日最新干货'),
-        ),
-      ),
+      drawer: GankDrawer(),
+      appBar: _buildAppBar(),
     );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      elevation: 0.0,
+      centerTitle: true,
+      title: Offstage(
+        offstage: false,
+        child: Text('今日最新干货'),
+      ),
+      actions: <Widget>[_buildActions()],
+    );
+  }
+
+  /// 构建右侧图标
+  IconButton _buildActions() {
+    return IconButton(
+        icon: Icon(
+          getActionsIcon(),
+          size: 22,
+          color: Colors.white,
+        ),
+        onPressed: null);
+  }
+
+  IconData getActionsIcon() {
+    return IconFont(0xe8a6);
   }
 }
---
+++ lib/widget/widget_gank_drawer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:gank_flutter/redux/app_state.dart';
+import 'package:gank_flutter/widget/widget_sizable_drawer.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+/// 自定义Drawer
+class GankDrawer extends StatefulWidget {
+  @override
+  _GankDrawerState createState() => _GankDrawerState();
+}
+
+class _GankDrawerState extends State<GankDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return SizableDrawer(
+      widthPercent: 0.75,
+      child: Column(
+        children: <Widget>[
+          StoreBuilder<AppState>(
+            builder: (context, state) => UserAccountsDrawerHeader(
+                  accountName: Text('sunxin'),
+                  accountEmail: Text('sunxin0630@gmail.com'),
+                  currentAccountPicture: GestureDetector(
+                    onTap: () {
+                      Fluttertoast.showToast(
+                          msg: 'taptap', gravity: ToastGravity.CENTER);
+                    },
+                    child: Container(
+                      width: 55,
+                      height: 55,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 1.0, color: Colors.white)),
+                      child: ClipOval(
+                        child: Image.asset('images/gank.png'),
+                      ),
+                      margin: EdgeInsets.zero,
+                    ),
+                  ),
+                ),
+          ),
+          MediaQuery.removePadding(
+              context: context,
+              child: Expanded(
+                  child: ListView(
+                children: <Widget>[
+                  Text('AAAAA'),
+                  Text('bbbbb'),
+                  Text('ccccc'),
+                ],
+              )))
+        ],
+      ),
+    );
+  }
+}
